3dc5136164724945a279eeb7da654a679acec2b7
almaz <n58773014@gmail.com>
2023-12-23 23:58:23 +0600

    A small revision of the code, namely, a transition is added when the number reaches 20

--- app/src/main/java/com/example/clickplusminusnumbers/MainActivity.kt
+++ app/src/main/java/com/example/clickplusminusnumbers/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.clickplusminusnumbers
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.clickplusminusnumbers.ui.NumderFragment
 
 class MainActivity : AppCompatActivity() {
     var agePlusMinus: Int = 1
-    var tvNumder: TextView = findViewById(R.id.tv_number)
+    lateinit var tvNumber: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +17,23 @@ class MainActivity : AppCompatActivity() {
 
         var btnMinus: Button = findViewById(R.id.btn_minus)
         var btnPlus: Button = findViewById(R.id.btn_plus)
+        tvNumber = findViewById(R.id.tv_number)
 
         btnPlus.setOnClickListener {
+            val age = tvNumber.text.toString().toIntOrNull() ?: 0
             ageInciriment()
             agePlusMinus++
-            
+            if (age == 20) {
+
+                val bundle = Bundle()
+                bundle.putString(TV_NUMBER_KEY, age.toString())
+                val numbetFragment = NumderFragment()
+                numbetFragment.arguments = bundle
+                val intent = Intent(this, numbetFragment::class.java)
+                startActivity(intent)
+            } else {
+
+            }
         }
 
         btnMinus.setOnClickListener {
@@ -31,8 +45,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ageInciriment() {
-        tvNumder.text = agePlusMinus.toString()
+        tvNumber.text = agePlusMinus.toString()
 
 
+    }
+
+    companion object {
+        const val TV_NUMBER_KEY = "tv.number.key"
     }
 }
--- app/src/main/java/com/example/clickplusminusnumbers/ui/NumderFragment.kt
+++ app/src/main/java/com/example/clickplusminusnumbers/ui/NumderFragment.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.clickplusminusnumbers.MainActivity.Companion.TV_NUMBER_KEY
 import com.example.clickplusminusnumbers.R
 
 class NumderFragment : Fragment() {
+    lateinit var tvNumbers: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         return inflater.inflate(R.layout.fragment_numder, container, false)
+        val tvNum = tvNumbers.findViewById<TextView>(R.id.tv_number)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bundle = Bundle()
+        val getTvNumber = bundle.getString(TV_NUMBER_KEY)
+        tvNumbers.text = getTvNumber
     }
 }
